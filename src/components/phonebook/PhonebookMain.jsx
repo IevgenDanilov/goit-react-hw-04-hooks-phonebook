a6b629365efe992294ef0ef0a49904d3a6263155
Yevhen Danilov <danilov.evgen@gmail.com>
2021-10-19 14:07:30 +0300

    hw4-2 compleated upds

--- src/components/phonebook/PhonebookMain.jsx
+++ src/components/phonebook/PhonebookMain.jsx
@@ -4,33 +4,31 @@ import PropTypes from "prop-types";
 import "./PhonebookMain.css";
 
 const Phonebook = ({ onSubmit }) => {
-  const [state, setState] = useState({
-    name: "",
-    number: "",
-  });
+  const [name, setName] = useState("");
+  const [number, setNumber] = useState("");
 
-  const handleChange = (e) => {
-    const { name, value } = e.currentTarget;
-    setState((prevState) => ({
-      ...prevState,
-      [name]: value,
-    }));
+  const handleChangeName = (e) => {
+    const { value } = e.currentTarget;
+    setName(value);
+  };
+
+  const handleChangeNumber = (e) => {
+    const { value } = e.currentTarget;
+    setNumber(value);
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
     const id = generate();
-    const { name, number } = state;
     const newContact = { id, name, number };
     onSubmit(newContact);
     reset();
   };
 
   const reset = () => {
-    setState({ name: "", number: "" });
+    setName("");
+    setNumber("");
   };
-
-  const { name, number } = state;
 
   return (
     <div>
@@ -41,7 +39,7 @@ const Phonebook = ({ onSubmit }) => {
             type="text"
             name="name"
             value={name}
-            onChange={handleChange}
+            onChange={handleChangeName}
             className="input"
             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
             title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
@@ -52,7 +50,7 @@ const Phonebook = ({ onSubmit }) => {
             type="tel"
             name="number"
             value={number}
-            onChange={handleChange}
+            onChange={handleChangeNumber}
             className="input"
             pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
             title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
