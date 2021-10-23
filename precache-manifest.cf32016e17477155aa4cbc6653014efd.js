f524169b49b34a7896a32780c9ec0e307d125a3f
Yevhen Danilov <danilov.evgen@gmail.com>
2021-10-23 18:21:45 +0300

    Updates

---
+++ precache-manifest.cf32016e17477155aa4cbc6653014efd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57ac508198a020313b328ec7a6af54fa",
+    "url": "/goit-react-hw-04-hooks-phonebook/index.html"
+  },
+  {
+    "revision": "c0cd2bb758cdfb87083a",
+    "url": "/goit-react-hw-04-hooks-phonebook/static/css/main.793c62f6.chunk.css"
+  },
+  {
+    "revision": "97ad3aeaf5b41d52be27",
+    "url": "/goit-react-hw-04-hooks-phonebook/static/js/2.dfc561b2.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/goit-react-hw-04-hooks-phonebook/static/js/2.dfc561b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0cd2bb758cdfb87083a",
+    "url": "/goit-react-hw-04-hooks-phonebook/static/js/main.8dbd8fdb.chunk.js"
+  },
+  {
+    "revision": "4dcff631daeb49597abe",
+    "url": "/goit-react-hw-04-hooks-phonebook/static/js/runtime-main.79e560b6.js"
+  }
+]);
